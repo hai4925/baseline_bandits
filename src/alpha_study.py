8600411a08e3056587a1beba712689b0c3b66937
Travis Dick <travis.barry.dick@gmail.com>
2013-08-18 20:21:17 -0600

    More tweaks to ParameterSettings and alpha_study.py

--- src/alpha_study.py
+++ src/alpha_study.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import math
 import numpy as np
 from multiprocessing import Pool
@@ -8,18 +9,20 @@ from param_study import ParameterSettings
 parameters = ParameterSettings({
     "num_arms": [10],
     "num_pulls": [20],
-    "num_runs": [1000],
+    "num_runs": [1000000],
     "step_size": np.logspace(0,2,20, base=2),
     "baseline": ["zero", "trcov", "value"]
 })
 
 def try_parameters(kwargs):
+    start_time = datetime.now()
     data = run_experiment(**kwargs)
     trial_means = data.mean(axis=1)
     trial_mean = trial_means.mean()
     trial_sigma = trial_means.std()
     trial_std_err = trial_sigma / math.sqrt(kwargs["num_runs"])
-    print kwargs["baseline"] + ", step_size=" + str(kwargs["step_size"]) + ": " + str(trial_mean)
+    time_elapsed = datetime.now() - start_time
+    print kwargs["baseline"] + ", step_size=" + str(kwargs["step_size"]) + ": " + str(time_elapsed)
     return (trial_mean, trial_std_err)
 
 if __name__ == "__main__":
--- src/param_study.py
+++ src/param_study.py
@@ -1,8 +1,10 @@
 from itertools import izip
+from types import FunctionType, DictType
 
 class ParameterSettings:
 
     def __init__(self, param_values):
+        self.param_values = param_values
         self.names = []
         self.values = []
         self.num_values = []
@@ -13,6 +15,9 @@ class ParameterSettings:
             self.num_values.append(len(values))
             self.num_settings *= len(values)
 
+    def get_values(self, param):
+        return self.param_values[param]
+
     def get(self, index):
         """ Get the ith parameter settings. """
         params = dict()
@@ -21,13 +26,33 @@ class ParameterSettings:
             index = int(index / m)
         return params
 
-    def indices(self, selector=None):
-        for i in xrange(self.num_settings):
-            if selector == None or selector(self.parameters(i)): yield i
+    def find_indices(self, selector=None):
+        if selector == None:
+            for i in xrange(self.num_settings): yield i
+        elif isinstance(selector, DictType):
+            for i in xrange(self.num_settings):
+                ps = self.get(i)
+                matches = True
+                for (k,v) in selector.iteritems():
+                    if not ps.has_key(k) or ps[k] != v:
+                        matches = False
+                        break
+                if matches: yield i
+        elif isinstance(selector, FunctionType):
+            for i in xrange(self.num_settings):
+                if selector(self.get(i)): yield i
 
-    def all(self, selector=None):
-        for i in self.indices(selector): 
-            yield self.get(i)
+    def list_indices(self, selector=None):
+        return list(self.find_indices(selector))
+
+    def find(self, selector=None):
+        for i in self.find_indices(selector): yield self.get(i)
+        
+    def list(self, selector=None):
+        return list(self.find(selector))
+
+    def all(self):
+        return self.find()
 
 
 
