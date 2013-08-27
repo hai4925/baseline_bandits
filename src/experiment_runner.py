4e5a5e7f8ca5157de0368a9e366d42ff8749c237
Travis Dick <travis.barry.dick@gmail.com>
2013-08-27 14:57:52 -0600

    Completely rewrote experiment_runner.py

--- src/experiment_runner.py
+++ src/experiment_runner.py
@@ -1,195 +1,192 @@
-from itertools import izip
+from itertools import ifilter, imap, izip
 import json
-from multiprocessing import Pool
+import multiprocessing
 import operator
 from os import path
-from subprocess import Popen, PIPE, STDOUT
+import re
+from subprocess import Popen, PIPE
 import sys
 from types import FunctionType, DictType
 
-def get_parameters(file_path):
-    param_config = json.load(file(file_path,"r"))
-    return ParameterProduct(param_config)
+################################################################################
+# Main function
+################################################################################
 
-def main(runner):
-    param_config_path = sys.argv[1]
+def main(experiment):
+    config_path = sys.argv[1]
+    out_dir = path.dirname(config_path)
+    param_index, options = load_config(config_path)
     
-    # Load the configuration file
-    param_config = json.load(file(param_config_path,"r"))
-    # Get resource limits out of the configuration file
-    resource_limits = ""
-    if param_config.has_key("__resource_limits__"):
-        resource_limits = param_config["__resource_limits__"]
-        del param_config["__resource_limits__"]
-    parameters = ParameterProduct(param_config)
+    modes = {
+        "local": run_local,
+        "torque": run_torque
+    }
+    mode = sys.argv[2]
+    modes[mode](experiment, out_dir, param_index, options)
+    
+################################################################################
+# Running modes
+################################################################################
 
-    # Function for outputting json in a consistent style
-    out_dir = path.dirname(param_config_path)
-    def write_output(basename, data):
-        json.dump(data, file(path.join(out_dir, basename), "w"),
-                  sort_keys=True, indent=2, separators=(",",": "))
+# ----- Run the experiment locally ----- #
 
-    run_type = sys.argv[2]
+class ArgWrapper:
+    def __init__(self, f): self.f = f
+    def __call__(self, args): return self.f(**args)
 
-    ############################################################################
-    # Run experiment locally
-    ############################################################################
-    if run_type == "local":
-        num_procs = int(sys.argv[3])
-        if num_procs == 1: 
-            results = map(RunnerWrapper(runner), parameters.all())
+def run_local(experiment, out_dir, param_index, options):
+
+    num_procs = int(sys.argv[3]) if len(sys.argv) >= 4 else 1
+    if num_procs == 1:
+        results = map(ArgWrapper(experiment), param_index.all())
+    else:
+        pool = multiprocessing.Pool(num_procs)
+        results = pool.map(ArgWrapper(experiment), param_index.all())
+    save_data(path.join(out_dir, "results.json"), results)
+
+# ----- Run the experiment using the TORQUE resource manager ----- #
+
+def run_torque(experiment, out_dir, param_index, options):
+
+    if len(sys.argv) == 3: # Request that all unfinished jobs be scheduled
+        # Get options
+        if options.has_key("__resource_limits__"):
+            resource_limits = options["__resource_limits__"]
         else:
-            pool = Pool(num_procs)
-            results = pool.map(RunnerWrapper(runner), parameters.all())
-        write_output("results.json", results)
-
-    ############################################################################
-    # Run experiment on a cluster controlled by torque
-    ############################################################################
-    if run_type == "torque":
-        # The script run by qsub
+            resource_limits = ""
+        # First, calculate which jobs need to run
+        def job_needed(i):
+            job_path = path.join(out_dir, "result_%d.json"%i)
+            return not path.exists(job_path)
+        needed_jobs = map(job_needed, param_index.all_ixs())
+        array_request = ",".join(map(range_to_str, true_ranges(needed_jobs)))
+        # Next, submit the jobs to TORQUE using the qsub command
         bash_script = \
         """
         #!/bin/bash
-        #PBS -k n -l %(resource_limits)s
+        #PBS -l %(resource_limits)s
         cd $PBS_O_WORKDIR
-        python %(script_name)s %(param_config_path)s torque_job $PBS_ARRAYID
-        """ % { "resource_limits": resource_limits,
-                "script_name": sys.argv[0], 
-                "param_config_path": param_config_path }
-        # Which jobs still need to run?
-        ranges = []
-        def already_done(i):
-            file_exists = path.exists(path.join(out_dir, "result_%d.json"%(i-1)))
-            return file_exists
-        range_start = 1
-        while range_start < parameters.num_settings:
-            while range_start < parameters.num_settings and already_done(range_start):
-                range_start += 1
-            range_end = range_start
-            while range_end <= parameters.num_settings and not already_done(range_end):
-                range_end += 1
-            ranges.append((range_start, range_end))
-            range_start = range_end
-        def range_to_str((a,b)):
-            if b-a <= 1: return str(a)
-            else: return str(a)+"-"+str(b-1)
-        array_jobs = ",".join(map(range_to_str, ranges))
-        print "Scheduling Jobs " + array_jobs
-
-        arguments = ["qsub",
-                     "-t", array_jobs,
-                     "-"]
-        p = Popen(arguments, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        python %(script_path)s %(config_path)s torque job $PBS_ARRAYID
+        """ % {
+            "resource_limits": resource_limits,
+            "script_path": sys.argv[0],
+            "config_path": sys.argv[1]
+        }
+        print "Scheduling jobs: " + array_request
+        p = Popen(["qsub", "-t", array_request, "-"], stdout=PIPE, stdin=PIPE)
         response = p.communicate(input=bash_script)
-        print "qsub response: "
-        print response
+        print "qsub response\n%s" % response
         p.wait()
 
-    ############################################################################
-    # A single torque job
-    ############################################################################
-    if run_type == "torque_job":
-        job_number = int(sys.argv[3]) - 1
-        result = runner(**parameters.get(job_number))
-        write_output("result_%d.json"%job_number, result)
-        exit(0)
+    elif sys.argv[3] == "job": # Run a specific job
+        job_number = int(sys.argv[4]) - 1
+        result = ArgWrapper(experiment)(param_index.get(job_number))
+        save_data(path.join(out_dir, "result_%d.json"%job_number), result)
 
-    ############################################################################    
-    # Gather the results of a torque run
-    ############################################################################    
+    elif sys.argv[3] == "gather": # Collect individual runs
+        try:
+            def load(i):
+                return load_data(path.join(out_dir, "result_%d.json"%i))
+            results = [load(i) for i in parameter_index.all_ixs()]
+            save_data(path.join(out_dir, "results.json"), results)
+        except e:
+            print "Not all jobs are done!"
+
+def true_ranges(l):
+    """ Given a list of booleans, returns a list of the true intervals. """
+    l.append(False)
+    ranges = []
+    start = 0
+    while start < len(l)-1:
+        while start < len(l)-1 and l[start] == False: start += 1
+        end = start
+        while end < len(l) and l[end] == True: end += 1
+        if end > start: ranges.append((start, end))
+        start = end
+    l.pop()
+    return ranges
+
+def range_to_str((start,end)):
+    if end - start == 1: 
+        return str(start+1)
+    else: 
+        return str(start+1)+"-"+str(end)
+
+################################################################################
+# Reading and writing to files
+################################################################################
+
+def load_config(path):
+    config = load_data(path)
+    params = dict()
+    options = dict()
+    for (k,v) in config.iteritems():
+        if re.match("__.*__", k): 
+            options[k] = v
+        else: 
+            params[k] = v
+    return (ParameterIndex(params), options)
+
+# these functions can be modified in case you don't want to use json
+
+def save_data(out_path, data):
+    json.dump(data, file(out_path, "w"), sort_keys=True, indent=2,
+               separators=(",", ": "))
+
+
+def load_data(in_path):
+    return json.load(file(in_path, "r"))
+
+################################################################################
+# ParameterIndex
+################################################################################
+
+class ParameterIndex:
     
-    if run_type == "torque_gather":
-        def load_result(job_number): 
-            return json.load(file(path.join(out_dir, "result_%d.json"%job_number), "r"))
-        results = map(load_result, parameters.all_ixs())
-        write_output("results.json", results)
-
-
-class ParameterProduct:
     def __init__(self, param_config):
-        """Constructs a new ParameterProduct. The input param_config is a
-        dictionary that maps parameter names to a collection of
-        values. This class allows the cross product of the provided
-        parameter settings to be iterated and efficiently indexed.
-        """
         self.param_config = param_config
-        self.names = list(param_config.iterkeys())
-        self.values = list(param_config.itervalues())
-        self.num_values = [len(vs) for vs in self.values]
-        # Sort parameters by name, since dictionary iterator order is unpredictable
-        self.names, self.values, self.num_values = \
-            [list(x) for x in zip(*sorted(zip(self.names, self.values, self.num_values)))]
-        self.num_settings = reduce(operator.mul, self.num_values)
+        self.names = sorted(param_config.keys())
+        self.values = [param_config[name] for name in self.names]
+        self.n_values = [len(vs) for vs in self.values]
+        self.n_settings = reduce(operator.mul, self.n_values)
+    
+    def values(self, param): return self.param_config["param"]
 
-    def get_values(self, parameter):
-        """ Get the possible values for a parameter. """
-        return self.param_config[parameter]
+    def num_settings(self): return self.n_settings
 
-    def num_settings(self): 
-        """ Get the number of parameter settings in the product. """
-        return self.num_settings
-
-    def get(self, i): 
-        """ Get the ith parameter setting. """
+    def get(self, ix):
         params = dict()
-        for (n, vs, m) in izip(self.names, self.values, self.num_values):
-            params[n] = vs[i%m]
-            i = int(i/m)
+        for (name, vs, n) in izip(self.names, self.values, self.n_values):
+            params[name] = vs[ix%n]
+            ix = int(ix/n)
         return params
 
-    def all(self):
-        """ Returns an iterator over all the parameter settings. """
-        return self.find()
+    def all_ixs(self, return_list=True): 
+        return self.find_ixs(return_list=return_list)
 
-    def all_ixs(self):
-        return self.find_ixs()
+    def all(self, return_list=True): 
+        return self.find(return_list=return_list)
 
-    def find_ixs(self, criterion=None): 
-        """ Iterates over the parameter indices matching a criterion. """
-        for i in xrange(self.num_settings):
-            if self.matches(criterion, self.get(i)): yield i
+    def find_ixs(self, criteria=None, return_list=True):
+        def matches(i):
+            params = self.get(i)
+            if criteria==None: return True
+            elif isinstance(criteria, DictType):
+                matches = True
+                for (k,v) in criteria.iteritems():
+                    if not params.has_key(k) or params[k] != v:
+                        matches = False
+                        break
+                return matches
+            elif isinstance(criteria, FunctionType): return criteria(params)
+        if return_list:
+            return filter(matches, xrange(self.num_settings()))
+        else:
+            return ifilter(matches, xrange(self.num_settings()))
 
-    def list_ixs(self, criterion=None): 
-        """ Same as find_ixs but returns a list instead of an iterator. """
-        return list(self.find_ixs(criterion))
-
-    def find(self, criterion=None):
-        """ Iterates over the parameter settings matching a criterion. """
-        for i in xrange(self.num_settings):
-            ps = self.get(i)
-            if self.matches(criterion, ps): yield ps
+    def find(self, filter=None, return_list=True):
+        if return_list: return map(self.get, self.find_ixs(filter))
+        else: return imap(self.get, self.find_ixs(filter, False))
         
-    def list(self, criterion=None): 
-        """ Same as find but returns a list instead of an iterator. """
-        return list(self.find(criterion))
-
-    def matches(self, criterion, ps):
-        """Checks whether some parameters match a criterion. The criterion may
-        be:
-          - None, in which case no filtering is done; 
-          - A dictionary mapping parameters to values, in which case only
-            parameter settings agreeing with the dictionary are matched.
-          - A function from parameters to booleans that does the matching.
-        """
-        if criterion == None: return True
-        elif isinstance(criterion, DictType):
-            matches = True
-            for (k,v) in criterion.iteritems():
-                if not ps.has_key(k) or ps[k] != v:
-                    matches = False
-                    break
-            return matches
-        elif isinstance(criterion, FunctionType):
-            return criterion(ps)
-
-
-class RunnerWrapper:
-    """ Wraps a function so that it takes a dictionary of named parameters. """
     
-    def __init__(self, f):
-        self.f = f
-        
-    def __call__(self, args):
-        return self.f(**args)
-        
+    
