0768debf45054c3205de0eae3248ef50abca41f6
Travis Dick <travis.barry.dick@gmail.com>
2013-08-29 21:26:24 -0600

    Experiment runner is now a standalone program

--- src/experiment.py
+++ src/experiment.py
@@ -1,5 +1,6 @@
-import numpy as np
 from cStringIO import StringIO
+import math
+import numpy as np
 from os import path
 import subprocess
 import sys
@@ -8,7 +9,7 @@ def experiment(value="last", baseline="zero", baseline_value="avg",
                baseline_step_size=0.1, step_size=0.1, num_arms=10, num_runs=10000, 
                num_pulls=200, seed=0, bandit_seed=1, arm_mean=0):
 
-    arguments = [path.join(path.dirname(sys.argv[0]), "experiment"),
+    arguments = [path.join(path.dirname(__file__), "experiment"),
                  "--value_estimate", value,
                  "--baseline", baseline,
                  "--baseline_value_estimate", baseline_value,
@@ -23,6 +24,24 @@ def experiment(value="last", baseline="zero", baseline_value="avg",
     
     result = StringIO(subprocess.check_output(arguments))
     return np.loadtxt(result)
+
+
+def parameter_study(**kwargs):
+    data = experiment(**kwargs)
+    means = data.mean(axis=1)
+    mean = means.mean()
+    sigma = means.std()
+    std_err = sigma / math.sqrt(kwargs["num_runs"])
+    return {"mean": mean, "std_err": std_err}
+
+
+def learning_curve(**kwargs):
+    data = experiment(**kwargs)
+    means = data.mean(axis=0)
+    sigmas = data.std(axis=0)
+    std_errs = sigmas / math.sqrt(kwargs["num_runs"])
+    return {"means": means.tolist(), "std_errs": std_errs.tolist()}
+    
 
 
 
